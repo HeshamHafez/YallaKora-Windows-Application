098da444caa61312796a9a292c1e74568be99eb8
HeshamHafez <heshamhafez94@gmail.com>
2020-11-05 05:58:00 +0200

    AddLeagueForm Done

---
+++ yallakora App/AddLeagueForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yallakora_App
+{
+    public partial class AddLeagueForm : Form
+    {
+        public AddLeagueForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeague_Click(object sender, EventArgs e)
+        {
+            sqlCommand1.CommandText = "INSERT INTO leagues (name) VALUES(' " + textBox1.Text + " ')";
+            sqlConnection1.Open();
+            sqlCommand1.ExecuteNonQuery();
+            sqlConnection1.Close();
+        }
+    }
+}
